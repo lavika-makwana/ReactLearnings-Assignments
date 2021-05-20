9432e10b2c0bf3cadfc0c7968cee30f5f3241b53
lavika-makwana <lavika.makwana03@gmail.com>
2021-05-20 12:17:10 +0530

    use of array to display data

---
+++ demo-application/src/components/Sdata.js
@@ -0,0 +1,10 @@
+const Sdata = [
+	{
+		sname: 'Dark',
+		imgsrc: 'https://wallpapercave.com/wp/wp4056410.jpg',
+		title: 'Netflix Original Series',
+		links: 'https://www.netflix.com/in/title/80990668?source=35'
+	}
+]
+
+export default Sdata;
--- demo-application/src/index.js
+++ demo-application/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './components/Cards'
+import Card from './components/Cards';
+import Sdata from './components/Sdata';
+
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // ReactDOM.render(<App />, document.getElementById('root') );
 ReactDOM.render(
 	<>
+		<h2>List Of Top Netflix Series</h2>
 		<Card
-			imgsrc='https://wallpapercave.com/wp/wp4056410.jpg'
-			title='A Netflix Original'
-			sname='Dark'
-			link='https://www.netflix.com/in/title/80990668?source=35'
+			imgsrc={Sdata[0].img}
+			title={Sdata[0].title}
+			sname={Sdata[0].sname}
+			imgsrc={Sdata[0].imgsrc}
 			/>
 	</>,
 	document.getElementById('root') 
