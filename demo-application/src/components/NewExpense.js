f9d547d413c5a4f8f0c8440528730b56b35520ba
lavika-makwana <lavika.makwana03@gmail.com>
2021-05-20 10:48:14 +0530

    taking the user input

---
+++ demo-application/src/components/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = () => {
+	return(
+		<div className='center_div'>
+		    <ExpenseForm />
+		</div>
+	)
+
+};
+
+export default NewExpense;
