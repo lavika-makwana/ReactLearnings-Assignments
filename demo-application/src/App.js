c681f58f831d63a8e55d749c12b92e095dd6153c
lavika-makwana <lavika.makwana03@gmail.com>
2021-05-23 00:25:54 +0530

    deleted the components and styling

--- demo-application/src/App.js
+++ demo-application/src/App.js
@@ -1,55 +1,13 @@
-import React, { useState } from 'react';
-import ToDoList from './components/ToDoList';
-import GreetMessage from './components/GreetMessage';
+import React from 'react';
 import './App.css';
 
 const App = () => {
   
-  const [inputList, setInputList] = useState(''); 
-
-  const [Items, setItems] = useState([]);
-
-  const itemEvents = (event) => {
-    setInputList(event.target.value);
-  };
-
-  const listOfItems = () => {
-    setItems((oldItems) => {
-      return [...oldItems, inputList];
-    });
-    setInputList('');
-  };
-
-
-  const deleteItem = (id) => {  
-    setItems((oldItems) => {
-      return oldItems.filter((arrElement, index) => {
-          return index !== id;
-      })
-    });
-  };
 
   return (
-    <div className='main_div'>
-      <GreetMessage></GreetMessage>
-      <div className='center_div'>
-        <h1>To Do List</h1>
-        <br/>
-        <input type='text' placeholder='Add a Item' onChange={itemEvents}/> 
-        <button onClick={listOfItems}> + </button>
-        <ol>
-          {Items.map((item_val, index) => { 
-            return <ToDoList
-                id= {index}
-                key= {index} 
-                text= {item_val}
-                onSelect= {deleteItem}
-            />;
-          })}
-          
-        </ol>
-      </div>
-    </div>
+    <>
+      <div><h1>Welcome to the base application</h1></div>
+    </>
   );
 };
   
