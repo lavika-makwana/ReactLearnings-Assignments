e3fc787e528b019b697d8b5ebb47104dfd812466
lavika-makwana <lavika.makwana03@gmail.com>
2021-05-23 00:16:21 +0530

    styling of the cards

--- demo-application/src/components/Cards.jsx
+++ demo-application/src/components/Cards.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Netflix.css';	
 
 function Card(props){
 	return(
-		<div className='cards'>
+		<div className='column'>
 			<div className='card'>
 				<img src={props.imgsrc} alt='picture' className='card__img' />
-				<div className='card__info'>
+				<div className='card__info'>{props.sname}
 					<span className='card__category'></span>
 					<h3 className='card__title'>{props.title}</h3>
 					<a href='' target='_blank'>
--- demo-application/src/index.js
+++ demo-application/src/index.js
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/Cards';
 import Sdata from './components/Sdata';
+import './components/Netflix.css'
 
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // ReactDOM.render(<App />, document.getElementById('root') );
 
-function ncard(val){
-	return(
-		<Card
-			imgsrc={val.imgsrc}
-			title={val.title}
-			sname={val.sname}
-			link={val.links}
-			/>
-	);
-};
 
 ReactDOM.render(
 	<>
-		<h2>List Of Top Netflix Series</h2>
-			{Sdata.map(ncard)}
+		<h2 className='header'>List Of Top Netflix Series</h2>
+			{ Sdata.map((val, index) => {
+				return (
+					<Card
+						imgsrc={val.imgsrc}
+						title={val.title}
+						sname={val.sname}
+						link={val.links}
+						/>
+				);
+			})}
 	</>,
 	document.getElementById('root') 
 );
